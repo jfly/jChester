04e7b854df94a07e32e6d7ea1328c3cf87fc46ed
Jeremy Fleischman <jeremyfleischman@gmail.com>
2014-12-06 03:08:52 -0800

    Commiting version v0.0.2

---
+++ jChester.js
@@ -0,0 +1,25 @@
+/*
+ *  jChester - v0.0.2
+ *  A time entry component for speedcubing solves.
+ *  https://github.com/jfly/jChester
+ *
+ *  Made by Jeremy Fleischman
+ *  Under GPL-3.0 License
+ */
+(function($) {
+
+  $.fn.jChester = function(options) {
+    var settings = $.extend({}, $.fn.jChester.defaults, options);
+
+    return this.css({
+      color: settings.color,
+      backgroundColor: settings.backgroundColor
+    });
+
+  };
+
+  $.fn.jChester.defaults = {
+    color: "green",//<<<
+  };
+
+}(jQuery));
